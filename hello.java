da579a4ecb9406ca7ce1bdf046546de9aeeb7d93
ruckerparkay <48200393+ruckerparkay@users.noreply.github.com>
2019-08-03 18:31:00 +0800

    helloworld

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+       public static void main(String[] args) {
+             
+	System.out.println("2222。");
+	System.out.println("111");
+	 System.out.println("我的第一个Java程序：Hello World。");
+	System.out.println("3333");
+       }
+}
